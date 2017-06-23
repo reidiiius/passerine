4dda70730e3d3db39b08c06674a99aeb411e9967
Reid Netterville III <reidiiius@gmail.com>
2017-06-23 17:39:55 -0500

    170623.0

--- ploceidae.tcl
+++ ploceidae.tcl
@@ -1,54 +1,99 @@
 #!/usr/bin/tclsh
 
-set Fn 25
-set Cn 0
-set Gn 35
-set Dn 10
-set An 45
-set En 20
-set Bn 55
+proc FingerBoard {qp str} {
 
-set circa [clock seconds]
+  set Bj 50
+  set Fn 25
+  set Cn 0
+  set Gn 35
+  set Dn 10
+  set An 45
+  set En 20
+  set Bn 55
+  set Fk 30
 
-proc headstock {str tune} {
-  return [
-    concat [string range $str $tune end] [string range $str 0 $tune-1]
-  ]
-}
+  set circa [clock seconds]
 
-proc beadgcf {clade circa flock Fn Cn Gn Dn An En Bn} {
-  puts ""
-  puts [format "\t%s" "$clade \b-b \b$circa"]
-  puts [format "\t%s" [headstock $flock $Fn]]
-  puts [format "\t%s" [headstock $flock $Cn]]
-  puts [format "\t%s" [headstock $flock $Gn]]
-  puts [format "\t%s" [headstock $flock $Dn]]
-  puts [format "\t%s" [headstock $flock $An]]
-  puts [format "\t%s" [headstock $flock $En]]
-  puts [format "\t%s" [headstock $flock $Bn]]
-  puts ""
-}
+  proc headstock {str peg} {
+    return [
+      concat [string range $str $peg end] [string range $str 0 [expr $peg - 1]]
+    ]
+  }
 
-proc eadgbe {clade circa flock Cn Gn Dn An En Bn} {
-  puts ""
-  puts [format "\t%s" "$clade \b-g \b$circa"]
-  puts [format "\t%s" [headstock $flock $En]]
-  puts [format "\t%s" [headstock $flock $Bn]]
-  puts [format "\t%s" [headstock $flock $Gn]]
-  puts [format "\t%s" [headstock $flock $Dn]]
-  puts [format "\t%s" [headstock $flock $An]]
-  puts [format "\t%s" [headstock $flock $En]]
-  puts ""
-}
+  proc beadgcf {qp circa str Fn Cn Gn Dn An En Bn} {
+    puts ""
+    puts [format "\t%s" "$qp \b-beadgcf- \b$circa"]
+    puts [format "\t%s" [headstock $str $Fn]]
+    puts [format "\t%s" [headstock $str $Cn]]
+    puts [format "\t%s" [headstock $str $Gn]]
+    puts [format "\t%s" [headstock $str $Dn]]
+    puts [format "\t%s" [headstock $str $An]]
+    puts [format "\t%s" [headstock $str $En]]
+    puts [format "\t%s" [headstock $str $Bn]]
+    puts ""
+  }
 
-proc cgdae {clade circa flock Cn Gn Dn An En} {
+  proc bfbfb {qp circa str Fn Bn} {
+    puts ""
+    puts [format "\t%s" "$qp \b-bfbfb- \b$circa"]
+    puts [format "\t%s" [headstock $str $Bn]]
+    puts [format "\t%s" [headstock $str $Fn]]
+    puts [format "\t%s" [headstock $str $Bn]]
+    puts [format "\t%s" [headstock $str $Fn]]
+    puts [format "\t%s" [headstock $str $Bn]]
+    puts ""
+  }
+
+  proc cgdae {qp circa str Cn Gn Dn An En} {
+    puts ""
+    puts [format "\t%s" "$qp \b-cgdae- \b$circa"]
+    puts [format "\t%s" [headstock $str $En]]
+    puts [format "\t%s" [headstock $str $An]]
+    puts [format "\t%s" [headstock $str $Dn]]
+    puts [format "\t%s" [headstock $str $Gn]]
+    puts [format "\t%s" [headstock $str $Cn]]
+    puts ""
+  }
+
+  proc eadgbe {qp circa str Cn Gn Dn An En Bn} {
+    puts ""
+    puts [format "\t%s" "$qp \b-eadgbe- \b$circa"]
+    puts [format "\t%s" [headstock $str $En]]
+    puts [format "\t%s" [headstock $str $Bn]]
+    puts [format "\t%s" [headstock $str $Gn]]
+    puts [format "\t%s" [headstock $str $Dn]]
+    puts [format "\t%s" [headstock $str $An]]
+    puts [format "\t%s" [headstock $str $En]]
+    puts ""
+  }
+
+  proc fkbjdn {qp circa str Bj Dn Fk} {
+    puts ""
+    puts [format "\t%s" "$qp \b-fkbjdn- \b$circa"]
+    puts [format "\t%s" [headstock $str $Dn]]
+    puts [format "\t%s" [headstock $str $Bj]]
+    puts [format "\t%s" [headstock $str $Fk]]
+    puts [format "\t%s" [headstock $str $Dn]]
+    puts [format "\t%s" [headstock $str $Bj]]
+    puts [format "\t%s" [headstock $str $Fk]]
+    puts ""
+  }
+
   puts ""
-  puts [format "\t%s" "$clade \b-c \b$circa"]
-  puts [format "\t%s" [headstock $flock $En]]
-  puts [format "\t%s" [headstock $flock $An]]
-  puts [format "\t%s" [headstock $flock $Dn]]
-  puts [format "\t%s" [headstock $flock $Gn]]
-  puts [format "\t%s" [headstock $flock $Cn]]
+
+  eadgbe $qp $circa $str $Cn $Gn $Dn $An $En $Bn
+
+  cgdae $qp $circa $str $Cn $Gn $Dn $An $En
+
+#  bfbfb $qp $circa $str $Fn $Bn
+
+#  beadgcf $qp $circa $str $Fn $Cn $Gn $Dn $An $En $Bn
+
+#  fkbjdn $qp $circa $str $Bj $Dn $Fk
+
   puts ""
+
+  unset circa Bj Fn Cn Gn Dn An En Bn Fk
+
 }
 
--- syrinx.tcl
+++ syrinx.tcl
@@ -4,39 +4,31 @@ source estrildidae.tcl
 source ploceidae.tcl
 
 if {$argc eq 0 || ![info exists oscines($argv)]} then {
-  list menu
-  foreach clade [lsort -ascii [array names oscines]] {
-    lappend menu $clade
+  set interim [list ]
+  foreach signat [lsort -ascii [array names oscines]] {
+    lappend interim $signat
   }
-  set nest [lsearch -all -inline $menu *$argv*]
-  if {[llength $nest] > 0} {
-    for {set i 0} {$i < [llength $nest]} {incr i} {
-      set pluma [lindex $nest $i]
+  set clade [lsearch -all -inline $interim *$argv*]
+  unset interim
+  if {[llength $clade] > 0} {
+    for {set i 0} {$i < [llength $clade]} {incr i} {
+      set pluma [lindex $clade $i]
       if {$i % 7 == 0} {
         puts -nonewline [format "\n\t%s" $pluma]
       } else {
         puts -nonewline [format "\t%s" $pluma]
       }
+      unset pluma
     }
     puts "\n"
   }
+  unset clade
   exit 0
 }
 
-set clade $argv
-set flock $oscines($clade)
+FingerBoard $argv $oscines($argv)
 
-  puts ""
-
-  eadgbe $clade $circa $flock $Cn $Gn $Dn $An $En $Bn
-
-  cgdae $clade $circa $flock $Cn $Gn $Dn $An $En
-
-#  beadgcf $clade $circa $flock $Fn $Cn $Gn $Dn $An $En $Bn
-
-  puts ""
-
-unset oscines clade circa flock Fn Cn Gn Dn An En Bn
+array unset oscines
 
 exit 0
 
