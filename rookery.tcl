e9b5f2849f73e8d75da6811ad344fcea733428d7
Reid Netterville III <reidiiius@gmail.com>
2022-10-18 00:13:36 -0500

    1666070016

--- rookery.tcl
+++ rookery.tcl
@@ -1,115 +1,125 @@
 #! /usr/bin/tclsh
 
-apply {{} {
-  # configure
-  set folder robin
-  set postdoc egretta.tcl
-  set pathway [file nativename [file join $folder $postdoc]]
+namespace eval Rookery {
 
-  file mkdir $folder
+  apply {{} {
+    # configure
+    set folder robin
+    set postdoc egretta.tcl
+    set pathway [file nativename [file join $folder $postdoc]]
 
-  if ![file isdirectory $folder] {
-    puts stderr "$folder not created"
-    exit 1
-  }
+    file mkdir $folder
 
-  # truncation
-  set capsule $pathway
-  set chandle [open $capsule w]
-  set content {}
-  puts -nonewline $chandle $content
-  flush $chandle
-  close $chandle
+    if ![file isdirectory $folder] {
+      puts stderr "$folder not created"
+      exit 1
+    }
 
-  if ![file isfile $pathway] {
-    puts stderr "$pathway not created"
-    exit 1
-  }
-
-  # shebang header
-  set capsule $pathway
-  set chandle [open $capsule w]
-  puts $chandle "#! /usr/bin/env tclsh\n"
-  flush $chandle
-  close $chandle
-
-  set capsule estrilda.tcl
-  set chandle [open $capsule r]
-  set content [read $chandle]
-  close $chandle
-
-  # append content
-  set capsule $pathway
-  set chandle [open $capsule a]
-  puts $chandle $content
-  flush $chandle
-  close $chandle
-
-  set capsule ploceus.tcl
-  set chandle [open $capsule r]
-  set content [read $chandle]
-  close $chandle
-
-  # append content
-  set capsule $pathway
-  set chandle [open $capsule a]
-  puts $chandle $content
-  flush $chandle
-  close $chandle
-
-  set capsule sturnus.tcl
-  set chandle [open $capsule r]
-  set content [read $chandle]
-  close $chandle
-
-  # append content
-  set capsule $pathway
-  set chandle [open $capsule a]
-  puts $chandle $content
-  flush $chandle
-  close $chandle
-
-  set capsule syrinx.tcl
-  set chandle [open $capsule r]
-  set content [read $chandle]
-  close $chandle
-
-  # split content lines to list
-  set splines [split $content "\n"]
-
-  # append content
-  set capsule $pathway
-  set chandle [open $capsule a]
-
-  foreach recline [lrange $splines 19 end] {
-    puts $chandle $recline
+    # truncation
+    set capsule $pathway
+    set chandle [open $capsule w]
+    set content {}
+    puts -nonewline $chandle $content
     flush $chandle
-  }
-  close $chandle
+    close $chandle
 
-  set capsule tyranni.tcl
-  set chandle [open $capsule r]
-  set content [read $chandle]
-  close $chandle
+    if ![file isfile $pathway] {
+      puts stderr "$pathway not created"
+      exit 1
+    }
 
-  # split content lines to list
-  set splines [split $content "\n"]
-
-  # append content
-  set capsule $pathway
-  set chandle [open $capsule a]
-
-  foreach recline [lrange $splines 19 end] {
-    puts $chandle $recline
+    # shebang header
+    set capsule $pathway
+    set chandle [open $capsule w]
+    puts $chandle "#! /usr/bin/env tclsh\n"
     flush $chandle
-  }
-  if {[catch {close $chandle} anomaly]} {
-      puts "$capsule failure: $anomaly"
-  }
+    close $chandle
 
-  file attributes $pathway -permissions u+x
+    set capsule estrilda.tcl
+    set chandle [open $capsule r]
+    set content [read $chandle]
+    close $chandle
 
-  # cleanup
-  unset anomaly capsule chandle content folder pathway postdoc recline splines
-}}
+    # append content
+    set capsule $pathway
+    set chandle [open $capsule a]
+    puts $chandle $content
+    flush $chandle
+    close $chandle
+
+    set capsule ploceus.tcl
+    set chandle [open $capsule r]
+    set content [read $chandle]
+    close $chandle
+
+    # append content
+    set capsule $pathway
+    set chandle [open $capsule a]
+    puts $chandle $content
+    flush $chandle
+    close $chandle
+
+    set capsule sturnus.tcl
+    set chandle [open $capsule r]
+    set content [read $chandle]
+    close $chandle
+
+    # append content
+    set capsule $pathway
+    set chandle [open $capsule a]
+    puts $chandle $content
+    flush $chandle
+    close $chandle
+
+    set capsule syrinx.tcl
+    set chandle [open $capsule r]
+    set content [read $chandle]
+    close $chandle
+
+    # split content lines to list
+    set splines [split $content "\n"]
+
+    # append content
+    set capsule $pathway
+    set chandle [open $capsule a]
+
+    foreach recline [lrange $splines 19 end] {
+      puts $chandle $recline
+      flush $chandle
+    }
+    close $chandle
+
+    set capsule tyranni.tcl
+    set chandle [open $capsule r]
+    set content [read $chandle]
+    close $chandle
+
+    # split content lines to list
+    set splines [split $content "\n"]
+
+    # append content
+    set capsule $pathway
+    set chandle [open $capsule a]
+
+    foreach recline [lrange $splines 19 end] {
+      puts $chandle $recline
+      flush $chandle
+    }
+    close $chandle
+
+    file attributes $pathway -permissions u+x
+    file stat $pathway estates
+
+    puts "\n\tSuccessfully wrote to $pathway\n"
+    puts "\tmodified: ${estates(mtime)}"
+    puts "\tinodenum: ${estates(ino)}"
+    puts "\tmodebits: ${estates(mode)}"
+    puts "\tfilesize: ${estates(size)}\n"
+
+    # cleanup
+    unset capsule chandle content estates
+    unset folder pathway postdoc recline splines
+  }}
+
+} ;# close Rookery
 
