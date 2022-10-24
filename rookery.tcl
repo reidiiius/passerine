6310b66d3e9d35b8198e9bc9333845580164aa5e
Reid Netterville III <reidiiius@gmail.com>
2022-10-24 14:34:56 -0500

    1666640096

--- rookery.tcl
+++ rookery.tcl
@@ -2,153 +2,191 @@
 
 namespace eval Rookery {
 
-  apply {{} {
-    # configure
-    set folder robin
-    set postdoc egretta.tcl
-    set pathway [file nativename [file join $folder $postdoc]]
+  # configure
+  variable cabinet
+  variable pathway
+  variable postdoc
+  variable section
 
+  set cabinet robin
+  set postdoc egretta.tcl
+  set pathway [file nativename [file join $cabinet $postdoc]]
+
+  proc transcribe {capsule destiny} {
+    if [file exists $capsule] then {
+      set chandle [open $capsule r]
+      set content [read $chandle]
+      close $chandle
+    } else {
+      puts stderr "$capsule not found"
+      exit 1
+    }
+
+    # append content
+    if [file exists $destiny] then {
+      set chapter [open $destiny a]
+      puts $chapter $content
+      flush $chapter
+      close $chapter
+    } else {
+      puts stderr "$destiny not found"
+      exit 1
+    }
+
+    unset capsule chandle
+    unset chapter content destiny
+    return
+  }
+
+  proc seditorial {capsule destiny {erasure 0}} {
+    if [file exists $capsule] then {
+      set chandle [open $capsule r]
+      set content [read $chandle]
+      close $chandle
+
+      # split content lines to list
+      set splines [split $content "\n"]
+    } else {
+      puts stderr "$capsule not found"
+      exit 1
+    }
+
+    # append content
+    if [file exists $destiny] then {
+      set chapter [open $destiny a]
+
+      foreach recline [lrange $splines $erasure end] {
+        puts $chapter $recline
+      }
+      flush $chapter
+      close $chapter
+    } else {
+      puts stderr "$destiny not found"
+      exit 1
+    }
+
+    unset capsule chandle chapter content
+    unset destiny erasure recline splines
+    return
+  }
+
+  # slurpermit and inscribe docstring
+  proc commentate {capsule destiny} {
+    if [file exists $capsule] then {
+      set chandle [open $capsule r]
+      set content [read $chandle]
+      close $chandle
+
+      # replace substrings then split content words to list
+      set swords [lrange [split [
+        string map {"\n\n" "\x20"} $content]] 4 end-1]
+    } else {
+      puts stderr "$capsule not found"
+      exit 1
+    }
+
+    # append content
+    if [file exists $destiny] then {
+      set chapter [open $destiny a]
+      set letters 0
+
+      puts $chapter "# \\"
+      foreach betoken $swords {
+        puts -nonewline $chapter [string cat $betoken "\x20"]
+        if {50 < [set letters [expr {$letters + [string length $betoken]}]]
+        } then {
+          puts $chapter "\\"
+          set letters 0
+        }
+      }
+      puts $chapter "\n"
+      flush $chapter
+      close $chapter
+    } else {
+      puts stderr "$destiny not found"
+      exit 1
+    }
+
+    unset betoken capsule chandle chapter
+    unset content destiny letters swords
+    return
+  }
+
+  # make directory and create file or
+  # overwrite and truncate existing file
+  apply {{folder destiny} {
     file mkdir $folder
 
-    if ![file isdirectory $folder] {
+    if [file isdirectory $folder] then {
+      set chandle [open $destiny w]
+
+      if [file isfile $destiny] then {
+        set content {}
+        puts -nonewline $chandle $content
+      } else {
+        puts stderr "$destiny not created"
+        exit 1
+      }
+
+      flush $chandle
+      close $chandle
+      unset chandle content
+    } else {
       puts stderr "$folder not created"
       exit 1
     }
 
-    # truncation
-    set capsule $pathway
-    set chandle [open $capsule w]
-    set content {}
-    puts -nonewline $chandle $content
-    flush $chandle
-    close $chandle
+    unset destiny folder
+    return
+  }} $cabinet $pathway
 
-    if ![file isfile $pathway] {
-      puts stderr "$pathway not created"
+  # shebang headline
+  apply {{destiny} {
+    if [file exists $destiny] then {
+      set chandle [open $destiny w]
+      set content "#! /usr/bin/env tclsh\n"
+      puts $chandle $content
+      flush $chandle
+      close $chandle
+
+      unset chandle content destiny
+    } else {
+      puts stderr "$destiny not found"
       exit 1
     }
 
-    # shebang header
-    set capsule $pathway
-    set chandle [open $capsule w]
-    puts $chandle "#! /usr/bin/env tclsh\n"
-    flush $chandle
-    close $chandle
-
-    # slurpermit
-    set capsule LICENSE.txt
-    set chandle [open $capsule r]
-    set content [read $chandle]
-    close $chandle
-
-    # replace substrings then split content words to list
-    set swords [lrange [split [
-      string map {"\n\n" "\x20"} $content]] 4 end-1]
-
-    # append content
-    set capsule $pathway
-    set chandle [open $capsule a]
-    set letters 0
-
-    puts $chandle "# \\"
-    foreach betoken $swords {
-      puts -nonewline $chandle [string cat $betoken "\x20"]
-      if {50 < [set letters [expr {$letters + [string length $betoken]}]]
-      } then {
-        puts $chandle "\\"
-        set letters 0
-      }
-    }
-    puts $chandle "\n"
-    flush $chandle
-    close $chandle
-
-    set capsule estrilda.tcl
-    set chandle [open $capsule r]
-    set content [read $chandle]
-    close $chandle
-
-    # append content
-    set capsule $pathway
-    set chandle [open $capsule a]
-    puts $chandle $content
-    flush $chandle
-    close $chandle
-
-    set capsule ploceus.tcl
-    set chandle [open $capsule r]
-    set content [read $chandle]
-    close $chandle
-
-    # append content
-    set capsule $pathway
-    set chandle [open $capsule a]
-    puts $chandle $content
-    flush $chandle
-    close $chandle
-
-    set capsule sturnus.tcl
-    set chandle [open $capsule r]
-    set content [read $chandle]
-    close $chandle
-
-    # append content
-    set capsule $pathway
-    set chandle [open $capsule a]
-    puts $chandle $content
-    flush $chandle
-    close $chandle
-
-    set capsule syrinx.tcl
-    set chandle [open $capsule r]
-    set content [read $chandle]
-    close $chandle
-
-    # split content lines to list
-    set splines [split $content "\n"]
-
-    # append content
-    set capsule $pathway
-    set chandle [open $capsule a]
-
-    foreach recline [lrange $splines 20 end] {
-      puts $chandle $recline
-    }
-    flush $chandle
-    close $chandle
-
-    set capsule tyranni.tcl
-    set chandle [open $capsule r]
-    set content [read $chandle]
-    close $chandle
-
-    # split content lines to list
-    set splines [split $content "\n"]
-
-    # append content
-    set capsule $pathway
-    set chandle [open $capsule a]
-
-    foreach recline [lrange $splines 20 end] {
-      puts $chandle $recline
-    }
-    flush $chandle
-    close $chandle
-
-    file attributes $pathway -permissions u+x
-    file stat $pathway estates
-
-    puts "\n\tSuccessfully wrote to $pathway\n"
-    puts "\tmodified: ${estates(mtime)}"
-    puts "\tinodenum: ${estates(ino)}"
-    puts "\tmodebits: ${estates(mode)}"
-    puts "\tfilesize: ${estates(size)}\n"
-
-    # cleanup
-    unset capsule chandle content estates folder letters
-    unset pathway postdoc recline splines swords betoken
     return
-  }}
+  }} $pathway
 
+  set section LICENSE.txt
+  commentate $section $pathway
+
+  set section estrilda.tcl
+  transcribe $section $pathway
+
+  set section ploceus.tcl
+  transcribe $section $pathway
+
+  set section sturnus.tcl
+  transcribe $section $pathway
+
+  set section syrinx.tcl
+  seditorial $section $pathway 20
+
+  set section tyranni.tcl
+  seditorial $section $pathway 20
+
+  file attributes $pathway -permissions u+x
+  file stat $pathway estates
+
+  puts "\n\tSuccessfully wrote to $pathway\n"
+  puts "\tmodified: ${estates(mtime)}"
+  puts "\tinodenum: ${estates(ino)}"
+  puts "\tmodebits: ${estates(mode)}"
+  puts "\tfilesize: ${estates(size)}\n"
+
+  # cleanup
+  unset cabinet estates
+  unset pathway postdoc section
 } ;# close Rookery
 
